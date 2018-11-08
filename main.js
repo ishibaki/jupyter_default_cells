a1423bde2dd741f45229d2612e32aa5990804862
ishibaki <iroirotomoki@gmail.com>
2018-11-08 12:59:08 +0900

    Format library list

--- main.js
+++ main.js
@@ -7,10 +7,34 @@ define([
       if (Jupyter.notebook.get_cells().length===1){
    //do your thing
         Jupyter.notebook.insert_cell_above('code', 0).set_text("\
-# Scientific libraries\nimport scipy\nimport numpy as np\nimport pandas as pd\n\n\
-# Graphic libraries\nimport matplotlib as mpl\nimport matplotlib.pyplot as plt\n# import japanize_matplotlib\n# import seaborn as sns\n# import holoviews as hv\n%matplotlib inline\n%config InlineBackend.figure_formats = {'png', 'retina'}\n\n\
-# Statistical libraries\n# import statsmodels.stats as stats\n# import statsmodels.stats.multicomp as multi\n# import statsmodels.formula.api as smf\n# import scikit_posthocs as sp\n\n\
-# Other libraries\n# import re\n# from datetime import datetime\n# import collections\n\n\
+# Scientific libraries\n\
+import scipy\n\
+import numpy as np\n\
+import pandas as pd\n\
+\n\
+# Graphic libraries\n\
+import matplotlib as mpl\n\
+import matplotlib.pyplot as plt\n\
+# import japanize_matplotlib\n\
+# import seaborn as sns\n\
+# import holoviews as hv\n\
+# from bokeh.io import output_notebook\n\
+# output_notebook()\n\
+# from bokeh.io import show\n\
+%matplotlib inline\n\
+%config InlineBackend.figure_formats = {'png', 'retina'}\n\
+\n\
+# Statistical libraries\n\
+# import statsmodels.stats as stats\n\
+# import statsmodels.stats.multicomp as multi\n\
+# import statsmodels.formula.api as smf\n\
+# import scikit_posthocs as sp\n\
+\n\
+# Other libraries\n\
+# import re\n\
+# from datetime import datetime\n\
+# import collections\n\
+\n\
 # Creating alias for magic commands\n%alias_magic t time");
         //Jupyter.notebook.insert_cell_above('code', 1).set_text("import something")
       }
